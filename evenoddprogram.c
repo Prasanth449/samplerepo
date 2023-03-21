ba368ee7847c34020ae6631726793fd83d7d2909
Prasanth449 <127648180+Prasanth449@users.noreply.github.com>
2023-03-21 15:14:47 +0530

    Add files via upload
    this is even odd program in c

---
+++ evenoddprogram.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  scanf("%d",a);
+  if (a%2==0)
+   {
+    printf("even number\n");
+    printf("this is prashanth");
+   }
+   else
+   {
+    printf("odd number");
+   }
+   return 0; 
+}
